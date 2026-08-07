aaac6db29e170d418305c198ece70d6a7817042f
MidNight services <juliansch187@gmail.com>
2026-08-07 11:01:57 +0200

    Delete mc_core directory

--- mc_core/config/config_npcblocker.lua
+++
@@ -1,9 +0,0 @@
----------------------------------------------------------------
--- MODUL: NpcBlocker  (eigenständige globale Werte, wie im Original)
----------------------------------------------------------------
-EnableNPCBlocker = true    -- NpcBlocker an/aus
-
-ZoneCenter = vector3(-326.8, 1327.61, 341.18)
-ZoneRadius = 230.0
-DebugNPCBlocker = false
-
--- mc_core/sql/autoAbschleppsystem_sql.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS vehicle_tracking (
-    plate VARCHAR(20) PRIMARY KEY,
-    owner VARCHAR(50) NOT NULL,
-    last_move INT NOT NULL,
-    status VARCHAR(20) DEFAULT 'active',
-    tow_time INT DEFAULT NULL
-);
